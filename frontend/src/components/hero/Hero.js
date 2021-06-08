5a0a5b8f78945b728017d71409c8d8137d15a8e6
Omas Ajiri <jicintech@gmail.com>
2021-06-08 08:20:41 +0100

    feat: hero settup

--- frontend/src/components/hero/Hero.js
+++ frontend/src/components/hero/Hero.js
@@ -1,18 +1,76 @@
 import React from "react";
+import { data } from "../../data";
+import "./hero.scss";
 
 function Hero(props) {
   return (
-    <div class="banner">
-      <div class="container">
-        <h1>Fabric Arena</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas eos
-          neque sunt in? Id, necessitatibus quos quisquam distinctio laudantium
-          fugiat?
-        </p>
-        <a href="#tours" class="btn ">
-          explore tours
-        </a>
+    <div className="banner">
+      <div className="slider">
+        <div className="mask">
+          <ul>
+            <li key={data[0].id} id="first" className="firstanimation">
+              <a href="#">
+                <img src={data[0].image} alt={data[0].heading} />
+              </a>
+              <div className="tooltip">
+                <h1>{data[0].heading}</h1>
+                <p>{data[0].description}</p>
+                <a href="#" className="btn btn-primary">
+                  Explore
+                </a>
+              </div>
+            </li>
+            <li key={data[1].id} id="first" className="secondanimation">
+              <a href="#">
+                <img src={data[1].image} alt={data[1].heading} />
+              </a>
+              <div className="tooltip">
+                <h1>{data[1].heading}</h1>
+                <p>{data[1].description}</p>
+                <a href="#" className="btn btn-primary">
+                  Explore
+                </a>
+              </div>
+            </li>
+            <li key={data[2].id} id="first" className="thirdanimation">
+              <a href="#">
+                <img src={data[2].image} alt={data[2].heading} />
+              </a>
+              <div className="tooltip">
+                <h1>{data[2].heading}</h1>
+                <p>{data[2].description}</p>
+                <a href="#" className="btn btn-primary">
+                  Explore
+                </a>
+              </div>
+            </li>
+            <li key={data[3].id} id="four" className="fourthanimation">
+              <a href="#">
+                <img src={data[3].image} alt={data[3].heading} />
+              </a>
+              <div className="tooltip">
+                <h1>{data[3].heading}</h1>
+                <p>{data[3].description}</p>
+                <a href="#" className="btn btn-primary">
+                  Explore
+                </a>
+              </div>
+            </li>
+            <li key={data[4].id} id="five" className="fifthanimation">
+              <a href="#">
+                <img src={data[4].image} alt={data[4].heading} />
+              </a>
+              <div className="tooltip">
+                <h1>{data[4].heading}</h1>
+                <p>{data[4].description}</p>
+                <a href="#" className="btn btn-primary">
+                  Explore
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="progress-bar"></div>
       </div>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+export const data = [
+  {
+    id: 1,
+    image: "/images/bg-1.jpg",
+    heading: "Fabric Arena",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 2,
+    image: "/images/bg-2.jpg",
+    heading: "Material Two",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+
+  {
+    id: 3,
+    image: "/images/bg-3.jpg",
+    heading: "Material Three",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 4,
+    image: "/images/bg-4.jpg",
+    heading: "Material Four",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 5,
+    image: "/images/bg-5.jpg",
+    heading: "Material Five",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
